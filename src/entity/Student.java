0f8dbc5aa47560f318cd70d49e15987425015eb6
animation <763678968@qq.com>
2019-07-29 22:10:46 +0800

    第一个Spring程序 创建Student.java

---
+++ src/entity/Student.java
@@ -0,0 +1,36 @@
+package entity;
+
+public class Student {
+    private int stuNo;
+    private String stuName;
+    private int stuAge;
+
+    public int getStuNo() {
+        return stuNo;
+    }
+
+    public void setStuNo(int stuNo) {
+        this.stuNo = stuNo;
+    }
+
+    public String getStuName() {
+        return stuName;
+    }
+
+    public void setStuName(String stuName) {
+        this.stuName = stuName;
+    }
+
+    public int getStuAge() {
+        return stuAge;
+    }
+
+    public void setStuAge(int stuAge) {
+        this.stuAge = stuAge;
+    }
+
+    @Override
+    public String toString() {
+        return this.stuNo + "," + this.stuName + "," + this.stuAge;
+    }
+}
