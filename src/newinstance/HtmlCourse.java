46e273ef1a415be9fa17af4b92e36d4c4fff440d
animation <763678968@qq.com>
2019-07-31 10:40:54 +0800

    简单工厂 创建课程HtmlCourse.java JavaCourse.java

---
+++ src/newinstance/HtmlCourse.java
@@ -0,0 +1,10 @@
+package newinstance;
+
+import java.util.Arrays;
+
+public class HtmlCourse implements ICourse {
+    @Override
+    public void learn() {
+        System.out.println("学习Html....");
+    }
+}
---
+++ src/newinstance/JavaCourse.java
@@ -0,0 +1,8 @@
+package newinstance;
+
+public class JavaCourse implements ICourse {
+    @Override
+    public void learn() {
+        System.out.println("学习Java...");
+    }
+}
