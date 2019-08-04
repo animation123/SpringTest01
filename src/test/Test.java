25978c025fba52b8a29b76e822be8561b619937f
animation <763678968@qq.com>
2019-08-04 13:34:26 +0800

    Test.java_modified

--- src/test/Test.java
+++ src/test/Test.java
@@ -54,7 +54,7 @@ public class Test {
 //         learnCourse();
 //         learnCourseWithFactory();
 //        learnCourseWithIOC();
-//        testDI();
-        collectionDemo();
+        testDI();
+//        collectionDemo();
     }
 }
