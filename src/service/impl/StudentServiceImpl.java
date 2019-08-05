5d3482af6b6664b560c31808783ed6587cbe3237
animation <763678968@qq.com>
2019-08-05 10:59:58 +0800

    StudentServiceImpl.java

--- src/service/impl/StudentServiceImpl.java
+++ src/service/impl/StudentServiceImpl.java
@@ -1,10 +1,11 @@
-package service;
+package service.impl;
 
 import dao.IStudentDao;
 import dao.impl.StudentDaoImpl;
 import entity.Student;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
+import service.IStudentService;
 
 public class StudentServiceImpl implements IStudentService {
     IStudentDao studentDao;
@@ -16,8 +17,14 @@ public class StudentServiceImpl implements IStudentService {
     @Transactional(readOnly=false, propagation= Propagation.REQUIRED)
     @Override
     public void addStudent(Student student) {
+//        studentDao = null; // 测试异常通知
         // if（该学生是否存在）
         // 增加其他...
         studentDao.addStudent(student);
+    }
+
+    @Override
+    public void deleteStudentByNo(int stuNo) {
+        System.out.println("模拟删除....");
     }
 }
